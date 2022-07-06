5a414c2185f966d16c05998c5b2ff84744f4da19
EpicCodeWizard <83260377+EpicCodeWizard@users.noreply.github.com>
2022-07-05 17:29:50 -0700

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,11 @@
+drop database lhd;
+create database ghw;
+create table challenges {
+  ChallengeName varchar(255),
+};
+insert into challenges (
+  ChallengeName
+)
+values (
+  'Hack With SQL'
+);
